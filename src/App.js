15ca38ea1e5c174e0033c65e018fe2ea392a0cb9
Cold <samedovvv00@gmail.com>
2022-12-28 20:59:14 +0400

    added components and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
+import AddTransaction from './components/AddTransaction'
+import Balance from './components/Balance'
 import Header from './components/Header'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionHistory from './components/TransactionHistory'
+
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionHistory />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   )
 }
 
---
+++ src/context/GlobalState.js
@@ -0,0 +1,18 @@
+import React, { createContext, useReducer } from 'react'
+import { AppReducer } from './AppReducer'
+
+const initialState = {
+  transactions: [{id: 1, text: 'Flower', amount: 300}],
+}
+
+export const GlobalContext = createContext(initialState)
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
