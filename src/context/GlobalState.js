750d0ac54be2fd0dd9e46874ef0d5bbddf4badb1
Cold <samedovvv00@gmail.com>
2023-01-11 14:02:10 +0400

    app completed

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,7 +2,7 @@ import React, { createContext, useReducer } from 'react'
 import { AppReducer } from './AppReducer'
 
 const initialState = {
-  transactions: [{id: 1, text: 'Flower', amount: 300}],
+  transactions: [],
 }
 
 export const GlobalContext = createContext(initialState)
@@ -10,8 +10,28 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    })
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    })
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   )
